7cbfac0e73f368044286388ee05076dc9a4aa780
winston <hey@winston.sh>
2025-04-21 08:09:54 +0200

    refactor: imports

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,15 +1,15 @@
 # SPDX-FileCopyrightText: 2025 winston <hey@winston.sh>
 # SPDX-License-Identifier: MIT
 
-import pathlib
-import shutil
-import subprocess
+from pathlib import Path
+from shutil import move, rmtree
+from subprocess import call, DEVNULL
 from typing import Callable as C
 
-info: C[[str], None] = lambda str: print(f"λ \033[1;33m{str}\033[1;0m")
-warn: C[[str], None] = lambda str: print(f"λ \033[1;31m{str}\033[1;0m")
-error: C[[str], None] = lambda str: print(f"λ \033[1;41m{str}\033[1;0m")
-nix_run: C[[list[str]], int] = lambda args: subprocess.call(
+info: C[[str], None] = lambda str: print(f"λ \033[1;36m{str}\033[1;0m")
+warn: C[[str], None] = lambda str: print(f"λ \033[1;33m{str}\033[1;0m")
+error: C[[str], None] = lambda str: print(f"λ \033[1;31m{str}\033[1;0m")
+nix_run: C[[list[str]], int] = lambda args: call(
     ["nix", "run", "--inputs-from", ".", f"nixpkgs#{args[0]}"] + (["--"] + args[1:])
 )
 
@@ -17,19 +17,19 @@ if __name__ == "__main__":
     if "{{ cookiecutter.use_reuse }}" == "True":
         info("Setting up REUSE...")
         # only keep the used licenses
-        for f in pathlib.Path("LICENSES").iterdir():
+        for f in Path("LICENSES").iterdir():
             if f.name not in ["CC0-1.0.txt", "{{ cookiecutter.license }}.txt"]:
                 f.unlink()
     else:
         info("Setting up the LICENSE...")
-        license = pathlib.Path("LICENSES/{{ cookiecutter.license }}.txt")
-        _ = shutil.move(license, "LICENSE")
+        license = Path("LICENSES/{{ cookiecutter.license }}.txt")
+        _ = move(license, "LICENSE")
 
-        shutil.rmtree(pathlib.Path("LICENSES"))
-        pathlib.Path("REUSE.toml").unlink()
+        rmtree(Path("LICENSES"))
+        Path("REUSE.toml").unlink()
 
     info("Initializing flake.lock...")
-    _ = subprocess.call(["nix", "flake", "lock"])
+    _ = call(["nix", "flake", "lock"])
 
     info("Running the initial hpack...")
     _ = nix_run(["hpack"])
@@ -38,23 +38,23 @@ if __name__ == "__main__":
     _ = nix_run(["fourmolu", "-i", "app", "src"])
 
     info("Running nix fmt")
-    _ = subprocess.call(["nix", "fmt", "--", "flake.nix"])
+    _ = call(["nix", "fmt", "--", "flake.nix"])
 
     if "{{ cookiecutter.is_executable }}" == "False":
-        shutil.rmtree(pathlib.Path("app"))
+        rmtree(Path("app"))
 
     if "{{ cookiecutter.is_library }}" == "False":
-        shutil.rmtree(pathlib.Path("src"))
+        rmtree(Path("src"))
 
     info("Initializing Git...")
-    _ = subprocess.call(["git", "init"], stdout=subprocess.DEVNULL)
-    _ = subprocess.call(["git", "add", "--all"], stdout=subprocess.DEVNULL)
-    _ = subprocess.call(
-        ["git", "commit", "-m", "feat: initial commit"], stdout=subprocess.DEVNULL
+    _ = call(["git", "init"], stdout=DEVNULL)
+    _ = call(["git", "add", "--all"], stdout=DEVNULL)
+    _ = call(
+        ["git", "commit", "-m", "feat: initial commit"], stdout=DEVNULL
     )
 
     info("Running initial pre-commit hooks...")
-    _ = subprocess.call(["nix", "flake", "check"])
+    _ = call(["nix", "flake", "check"])
 
     info("Preparing devShell...")
-    _ = subprocess.call(["nix", "develop", "-c", "true"])
+    _ = call(["nix", "develop", "-c", "true"])
--- hooks/pre_prompt.py
+++ hooks/pre_prompt.py
@@ -2,16 +2,16 @@
 # SPDX-License-Identifier: MIT
 
 import sys
-import subprocess
+from subprocess import run
 from typing import Callable as C
 
-info: C[[str], None] = lambda str: print(f"λ \033[1;33m{str}\033[1;0m")
-warn: C[[str], None] = lambda str: print(f"λ \033[1;31m{str}\033[1;0m")
-error: C[[str], None] = lambda str: print(f"λ \033[1;41m{str}\033[1;0m")
+info: C[[str], None] = lambda str: print(f"λ \033[1;36m{str}\033[1;0m")
+warn: C[[str], None] = lambda str: print(f"λ \033[1;33m{str}\033[1;0m")
+error: C[[str], None] = lambda str: print(f"λ \033[1;31m{str}\033[1;0m")
 
 if __name__ == "__main__":
     try:
-        _ = subprocess.run(["nix", "--version"], capture_output=True, check=True)
+        _ = run(["nix", "--version"], capture_output=True, check=True)
     except Exception:
         error("Nix is not installed. You can't use this cookiecutter without it.")
         sys.exit(1)
